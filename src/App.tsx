4f61efe3c8782c67332115f539b5061b84a72758
BlackiePearlJoobi <katze526reiswein@gmail.com>
2025-07-18 19:06:04 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import Form from "./components/Form";
+import data from "./social.json";
+
+function App() {
+  const basePath = import.meta.env.BASE_URL;
+
+  return (
+    <div className="wrapper">
+      <main>
+        <section>
+          <img
+            src={`${basePath}/assets/images/logo.svg`}
+            alt="PING logo"
+            width={53.58}
+            height={15.24}
+            className="logo"
+          ></img>
+          <div className="heading">
+            <h1>
+              <span>We are launching</span> soon!
+            </h1>
+            <p>Subscribe and get notified</p>
+          </div>
+          <Form></Form>
+        </section>
+        <img
+          src={`${basePath}/assets/images/illustration-dashboard.png`}
+          alt="Dashboard illustration"
+          width={319.94}
+          height={191.94}
+          className="dashboard"
+        ></img>
+      </main>
+      <footer>
+        <ul>
+          {data.map((social) => (
+            <li>
+              <a key={social.id} href="#">
+                <img
+                  src={`${basePath}/${social.image}`}
+                  alt={`${social.name} icon`}
+                  width={24}
+                  className="social-icon"
+                ></img>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <p>© Copyright Ping. All rights reserved.</p>
+        <div className="attribution">
+          Challenge by&nbsp;
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            aria-label="Learn more about challenges on frontendmentor.io"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by&nbsp;
+          <a
+            href="https://www.frontendmentor.io/profile/BlackiePearlJoobi"
+            target="_blank"
+            aria-label="Visit Kohta Kumazaki's developer profile on frontendmentor.io"
+          >
+            Kohta Kumazaki
+          </a>
+          .
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,49 @@
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  mail: string;
+};
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    window.alert(
+      `🎉 You're in!\n\nUpdates are heading to ${data.mail}.\n\nStay tuned!`,
+    );
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset>
+        <legend>Contact Information</legend>
+        <label htmlFor="mail">Email Address</label>
+        <div className="input-container">
+          <input
+            type="email"
+            id="mail"
+            autoComplete="email"
+            placeholder="Your email address..."
+            {...register("mail", {
+              required: "Email is required",
+              pattern: {
+                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.\w{2,}$/i,
+                message: "Please provide a valid email address",
+              },
+            })}
+          ></input>
+          {errors.mail && <p>{errors.mail.message}</p>}
+        </div>
+        <button type="submit">Notify Me</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
