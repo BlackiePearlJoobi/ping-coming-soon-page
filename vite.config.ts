63b06f0bf18b8af70c05fbd25caafc837326c4bc
BlackiePearlJoobi <katze526reiswein@gmail.com>
2025-07-18 19:13:08 -0500

    Corrected base repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ping-coming-soon-page.git",
+  base: "/ping-coming-soon-page",
 });
